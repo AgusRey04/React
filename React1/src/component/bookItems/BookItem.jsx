902a32d315c0764f11adb9c2e103ab33d1fe863f
AgusRey04 <reymundez37@gmail.com>
2024-03-27 17:14:23 -0300

    deleted:    React1/src/App.css modified:   React1/src/App.jsx new file:   React1/src/component/bookItems/BookItem.css new file:   React1/src/component/bookItems/BookItem.jsx modified:   React1/src/index.css modified:   React1/src/main.jsx

---
+++ React1/src/component/bookItems/BookItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BookItem = () => {
+  return (
+    <div>
+          <div> BookItem</div>
+        <h2>Titulo</h2>
+    <h3>Autor</h3>
+  < div> cant</div>
+    </div>
+  )
+}
+
+export default BookItem
